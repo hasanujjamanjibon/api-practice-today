2d8bdf84aee262f7697e718b342723d565c49f7d
hasanujjamanjibon <hasanujjamanjibon2@gmail.com>
2023-03-01 21:53:11 +0600

    update

--- api-practice03/js/main.js
+++ api-practice03/js/main.js
@@ -1,40 +1,38 @@
 const countryContainer = document.getElementById("country-container");
+const nextBtn = document.getElementById("btn-showNext");
+const spinner = document.getElementById("spinner");
+
 const baseURL = "https://restcountries.com/v2";
-let type = "region";
+let apiType = "region";
 let searchText = "asia";
+let limit = 12;
+
+const getApiURL = () => `${baseURL}/${apiType}/${searchText}`;
 
 const loadData = async (isShowNext) => {
-  const URL = `${baseURL}/${type}/${searchText}`;
-  console.log(URL);
+  const URL = getApiURL();
   const res = await fetch(URL);
-  const data = await res.json();
-  // console.log(data);
+  let data = await res.json();
+  data = isShowNext ? data.slice(limit, data.length) : data.slice(0, limit);
 
-  if (isShowNext) {
-    document.getElementById("btn-showNext").classList.add("hidden");
-    country = displayAllData(data.slice(12, data.length));
-    return country;
-  }
-  document.getElementById("btn-showNext").classList.remove("hidden");
-  country = displayAllData(data.slice(0, 12));
+  displayAllData(data);
+  // if 'isShowNext' is true, add "hidden" class, if false remove it.
+  nextBtn.classList.toggle("hidden", !!isShowNext);
 };
 
 const displayAllData = (data) => {
   countryContainer.innerHTML = "";
-  data.forEach((data) => {
-    const { cca2, name, population, flags, alpha2Code } = data;
-
-    const countryDiv = document.createElement("div");
-    countryDiv.classList.add("card", "w-full", "bg-base-100", "shadow-2xl");
-    countryDiv.innerHTML = `
-          <figure>
+  data.forEach(({ cca2, name, population, flags, alpha2Code }) => {
+    countryContainer.innerHTML += `
+    <div class="card w-full bg-base-100 shadow-2xl">
+        <figure>
           <img class="w-[100%] h-[200px]" src="${
             flags
               ? flags.png
               : "https://i.ytimg.com/vi/ASeHQ-nVzvY/maxresdefault.jpg"
           }" alt="" />
-          </figure>
-          <div class="card-body text-center">
+        </figure>
+        <div class="card-body text-center">
           <h2 class="card-title justify-center font-bold  text-2xl">${name}</h2>
           <p class="card-title justify-center  font-bold text-xl">Population : ${population} </p>
           <div class="card-actions justify-center">
@@ -42,22 +40,23 @@ const displayAllData = (data) => {
             alpha2Code ? alpha2Code : cca2
           }')" class="btn btn-secondary">More Details</label>
           </div>
-          </div>
+        </div>
+    </div>
     `;
-    countryContainer.appendChild(countryDiv);
   });
+
   spinner(false);
 };
+
 // Modal
 const showData = async (code) => {
-  const URL = `https://restcountries.com/v2/alpha/${code}`;
+  const URL = `${baseURL}/alpha/${code}`;
   const res = await fetch(URL);
-  const data = await res.json();
-  // console.log(data);
-  const { flags, region, area, nativeName, numericCode, independent } = data;
+  const { flags, region, area, nativeName, numericCode, independent } =
+    await res.json();
+
   modalBox.innerHTML = `
         <div class="modal-box">
-          
           <figure>
               <img src="${
                 flags
@@ -72,28 +71,17 @@ const showData = async (code) => {
           <h3  class="font-bold text-lg pt-1">Native Name : ${nativeName}</h3>
           <h3  class="font-bold text-lg pt-1">Independent : ${independent}</h3>
          
-
           <div class="modal-action">
               <label for="my-modal-6" class="btn btn-primary">Close</label>
           </div>
         </div> 
   `;
 };
+
 // spinner
-const spinner = (isSpin) => {
-  if (isSpin) {
-    document.getElementById("spinner").classList.add("flex");
-    document.getElementById("spinner").classList.remove("hidden");
-  } else {
-    document.getElementById("spinner").classList.add("hidden");
-    document.getElementById("spinner").classList.remove("flex");
-  }
-};
-const getTextByFunction = (searchTextElement) => {
-  document.getElementById("btn-showNext").classList.remove("hidden");
-  spinner(true);
-  searchText = searchTextElement;
-  loadData();
+const showSpinner = (isSpin) => {
+  spinner.classList.toggle("flex", !!isSpin);
+  spinner.classList.toggle("hidden", !isSpin);
 };
 
 // Lang api fetch from file
@@ -102,24 +90,29 @@ const fecthLangApi = async () => {
   const data = await res.json();
   loadLang(data);
 };
+
 const loadLang = (data) => {
   data.forEach((data) => {
     const { code } = data;
 
     const langSelectElement = document.getElementById("lang-option");
     langSelectElement.innerHTML += `
-    <option  onclick="getTextByFunction('${code}')" value='${code}'>${code}</option>
+    <option value='${code}'>${code}</option>
     `;
   });
   spinner(false);
 };
+
 const val = () => {
   searchText = document.getElementById("select_id").value;
   loadData();
 };
 
 const language = () => {
+  spinner(true);
   searchText = document.getElementById("lang-option").value;
+  document.getElementById("btn-showNext").classList.remove("hidden");
+  apiType = "lang";
   loadData();
 };
 
